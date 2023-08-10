93a0f2b395e4c04c29aa5706c1b29a441c302739
alwihabsyi <alwihbsyi.ah@gmail.com>
2023-08-10 19:33:22 +0700

    commit

--- app/src/main/java/com/alwihabsyi/shupashup/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/alwihabsyi/shupashup/ui/auth/LoginFragment.kt
@@ -5,14 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import br.com.simplepass.loadingbutton.customViews.CircularProgressButton
 import com.alwihabsyi.shupashup.R
 import com.alwihabsyi.shupashup.databinding.FragmentLoginBinding
 import com.alwihabsyi.shupashup.ui.shop.ShoppingActivity
